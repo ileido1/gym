8c91bc5a09c75fb38122c98339abb8114467f249
Carlos H <carlosluishc@hotmail.com>
2017-09-02 19:12:02 -0400

    Probando

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
   def index
-     @payment = Payment.all.group(:client_id).order("fecha_payment DESC")
+     @payment = Payment.group_by(:client_id).order("fecha_payment DESC")
      @timenow = Date.today - 1.month
   end
 end
