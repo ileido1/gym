786a36213148a8befc84054978b5197d99f7c7b1
Carlos H <carlosluishc@hotmail.com>
2017-09-04 16:40:44 -0400

    again

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!
   def index
      @payment = Payment.select("DISTINCT ON (client_id) * ").order("client_id, fecha_payment DESC")
+     # sqlite group(client_id).order("fecha_payment DESC")
      @timenow = Date.today - 1.month
   end
 end
