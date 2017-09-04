dc5df945c1f2f2b4eb68d1b1ec68d65b5a524f89
Carlos H <carlosluishc@hotmail.com>
2017-09-04 16:02:10 -0400

    Prueba

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
   def index
-     @payment = Payment.order("fecha_payment DESC")
+     @payment = Payment.select("DISTINCT ON (:client_id) * ").order("fecha_payment DESC")
      @timenow = Date.today - 1.month
   end
 end
